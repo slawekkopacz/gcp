2b8dc21626819e69c5d05993f2d4a54a251098a5
Slawek Kopacz <skopacz@objectivity.co.uk>
2020-07-30 11:52:59 +0200

    app.yaml added; Initial firestore integration (works locally)

---
+++ GcpPoc/CloudFirestoreService/CloudFirestoreClient.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using Google.Cloud.Firestore;
+
+namespace CloudFirestoreService
+{
+    public class CloudFirestoreClient
+    {
+        private const string projectId = "gcp-poc-284907";
+
+        public async IAsyncEnumerable<string> GetBooks()
+        {
+            //Uncomment to test locally
+            //string credential_path = @"C:\Users\skopacz\Desktop\gcp-poc-e3e9510aba44.json";
+            //System.Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", credential_path);
+
+            FirestoreDb db = FirestoreDb.Create(projectId);
+            Console.WriteLine("Created Cloud Firestore client with project ID: {0}", projectId);
+
+            CollectionReference collectionReference = db.Collection("books");
+            QuerySnapshot snapshot = await collectionReference.GetSnapshotAsync();
+            foreach (DocumentSnapshot document in snapshot.Documents)
+            {
+                Dictionary<string, object> documentDictionary = document.ToDictionary();
+
+                yield return
+                    $@"Document id: {document.Id}; Book title: {documentDictionary["title"]}; Book author: {documentDictionary["author"]}";
+            }
+        }
+    }
+}
---
+++ GcpPoc/GcpWebApi/Controllers/BooksController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CloudFirestoreService;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GcpWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IEnumerable<string>> Get()
+        {
+            var firestoreClient = new CloudFirestoreClient();
+            var books = await firestoreClient.GetBooks().ToListAsync();
+            return books;
+        }
+    }
+}
